72a78246560760044723bf4fd005b863529c28b9
art <artem11.ralko@gmail.com>
2023-12-07 18:59:07 +0200

    drawing with the turtle

--- lesson_9/turtle_letter_S.py
+++ lesson_9/turtle_letter_S.py
@@ -1,10 +1,12 @@
 from turtle import *
 
-
+# Create a turtle for drawing
 t = Turtle()
 t.shape('turtle')
 t.color('#9e160d')
 t.pensize(7)
+
+# Drawing the letter S
 t.lt(180)
 t.fd(99)
 t.lt(90)
@@ -15,7 +17,5 @@ t.rt(90)
 t.fd(99)
 t.rt(90)
 t.fd(99)
-
-
 
 done()
